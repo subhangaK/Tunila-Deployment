998e221f2fb16d99247163a7776f8bf73b39c082
subhangaK <subhangakhanal@gmail.com>
2025-03-06 18:32:13 +0545

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -33,6 +33,7 @@ export const register = async (req, res)=>{
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
